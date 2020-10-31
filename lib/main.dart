94bacdc4cc247df319c37081336f582eadec92a0
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-10-31 15:36:46 +0100

    Updated MyApp.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,4 @@
+import 'package:catfeeder/widgets/my_app.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    );
-  }
-}
