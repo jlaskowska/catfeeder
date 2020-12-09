070c7bd184f70312579d5655f4b367e0cf73dd14
jlaskowska <58981559+jlaskowska@users.noreply.github.com>
2020-12-09 13:51:31 +0100

    Updated backend.

--- lib/modules/backend/backend.dart
+++ lib/modules/backend/backend.dart
@@ -1,5 +1,7 @@
 library backend;
 
+export 'src/services/firebase_backend_service.dart';
+export 'src/services/i_backend_service.dart';
 export 'src/models/user.dart';
 export 'src/models/cat.dart';
 export 'src/models/feed_event.dart';
--- lib/modules/backend/src/services/i_backend_service.dart
+++ lib/modules/backend/src/services/i_backend_service.dart
@@ -2,10 +2,22 @@ import 'package:catfeeder/modules/backend/backend.dart';
 
 abstract class IBackendService {
   Future<void> initialize();
-  Stream<List<FeedEvent>> getFeedEvents();
-  Stream<List<Cat>> getCats();
-  User getUser();
-  Future<bool> setUser();
-  Future<bool> setCat();
-  Future<bool> setFeedEvent();
+
+  Future<bool> addUser(User user);
+
+  Future<User> getUser(String userId);
+
+  Future<bool> addCat(Cat cat);
+
+  Future<bool> updateCat(Cat cat);
+
+  Future<Cat> getCat(String catId);
+
+  Future<List<Cat>> getCatsForUser(String userId);
+
+  Stream<List<FeedEvent>> getFeedEventsForUser(String userId);
+
+  Stream<List<FeedEvent>> getFeedEventsForCat(String catId);
+
+  Future<bool> addFeedEvent(FeedEvent feedEvent);
 }
